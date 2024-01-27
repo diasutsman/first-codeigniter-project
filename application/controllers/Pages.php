24af5ac23dd80b3a121f4b95ec587fae8a88dc0e
Dias Utsman <utsmand91@gmail.com>
2024-01-27 11:12:33 +0700

    Fix checking file doesnt exists negated

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,7 +4,7 @@ class Pages extends CI_Controller
 
     public function view($page = 'home')
     {
-        if (file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
+        if (!file_exists(APPPATH . 'views/pages/' . $page . '.php')) {
             show_404();
         }
 
